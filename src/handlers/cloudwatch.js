18f1171de3e7a83f5f68af3d3e0cf84949246a66
otsu81 <c@otsu.se>
2020-11-26 10:13:54 +0100

    remove unnecessary dependencies

--- src/handlers/cloudwatch.js
+++ src/handlers/cloudwatch.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const AWS = require('aws-sdk');
 const getRoleCredentials = require('./getRoleCreds');
 const getActiveAccountIds = require('./organizations');
--- src/handlers/getRoleCreds.js
+++ src/handlers/getRoleCreds.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const AWS = require('aws-sdk');
 
 let getRoleCredentials = async (accountId, roleName, sessionName) => {
