1fe6552337713fab123cf2dcebf5fdad5c0cd1ba
otsu81 <c@otsu.se>
2020-11-26 10:08:48 +0100

    iam permissions, removing incorrect invocation

---
+++ .aws-sam/build/cloudwatchRetentionSetter/src/handlers/getRoleCreds.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const AWS = require('aws-sdk');
+
+let getRoleCredentials = async (accountId, roleName, sessionName) => {
+    // Set the region
+    AWS.config.update({region: process.env.DEFAULT_REGION});
+    const arn = 'arn:aws:iam::' + accountId + ':role/' + roleName
+    var roleToAssume = {RoleArn: arn,
+                        RoleSessionName: sessionName,
+                        DurationSeconds: 900,};
+
+    // Create the STS service object
+    var roleCreds;
+    var sts = new AWS.STS();
+    const response = await sts.assumeRole(roleToAssume).promise();
+    // console.log(accountId, response)
+    return response;
+}
+
+module.exports = getRoleCredentials;
